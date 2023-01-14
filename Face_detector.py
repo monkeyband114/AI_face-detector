733593ac85e5a4022361c23277d5697e7b678fc8
Michael Voke <gadocer54@gmail.com>
2023-01-14 14:16:49 +0100

    modified:   Face_detector.py

--- Face_detector.py
+++ Face_detector.py
@@ -2,20 +2,35 @@ import cv2
 
 facetrain = cv2.CascadeClassifier('frintalface.xml')
 
-amg = cv2.imread('thaks.jpg')
+# amg = cv2.imread('wera.jpg')
+webcam = cv2.VideoCapture(0)
 
-gray_man = cv2.cvtColor(amg, cv2.COLOR_BGR2GRAY)
+while True:
+    
+    sucessful_frame_read, frame = webcam.read()
+    
+    gray_man = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    trainmike = facetrain.detectMultiScale(gray_man)
+    
+    for (x, y, w, h) in  trainmike:
+         cv2.rectangle(frame, (x, y), (x+w, y+h), (225, 225,0), 2)
+    
+    cv2.imshow('i be dead', frame)
+    cv2.waitKey(1)
+
+# gray_man = cv2.cvtColor(webcam, cv2.COLOR_BGR2GRAY)
+
+# trainmike = facetrain.detectMultiScale(gray_man)
 
 
-trainmike = facetrain.detectMultiScale(gray_man)
 
-(x, y, w, h) = trainmike[0]
+# for (x, y, w, h) in  trainmike:
+#         cv2.rectangle(amg, (x, y), (x+w, y+h), (225, 225,0), 2)
 
-cv2.rectangle(amg, (x, y), (x+w, y+h), (225, 225,0), 2)
+# print(trainmike)
 
-print(trainmike)
+# cv2.imshow('i be dead', amg)
+# cv2.waitKey()
 
-cv2.imshow('i be dead', amg)
-cv2.waitKey()
-
-print("code Completed")  
+# print("code Completed")  
